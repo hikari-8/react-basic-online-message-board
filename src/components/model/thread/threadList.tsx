f76a186b927e9bf69f94129ef0a3665be895749e
hikari-8 <kanbarusuruga0127@gmail.com>
2023-05-26 22:29:38 +0900

    fix/each child in a list should have a unique key 🔑

--- src/components/model/thread/threadList.tsx
+++ src/components/model/thread/threadList.tsx
@@ -4,18 +4,21 @@ import { Thread } from "../../../type/model";
 
 type ThreadListProps = {
   threads: Thread[]
-
 }
-
 
 export const ThreadList:React.FC<ThreadListProps> =({threads})=> {
   return (
-    <div className="flex flex-col justify-center w-full">
+    <ul className="flex flex-col justify-center w-full">
           {
             threads.map((thread) => {
-              return <ThreadListCard thread={thread} />
+              return (
+              <li key={thread.id}>
+                <ThreadListCard thread={thread} />
+              </li>
+
+              )
               })
           }
-    </div>
+    </ul>
   )
 }
--- src/components/model/thread/threadListCard.tsx
+++ src/components/model/thread/threadListCard.tsx
@@ -10,7 +10,7 @@ export const ThreadListCard:React.FC<ThreadListCardProps> =({thread})=> {
     <>
       {
         thread && 
-        <div className="bg-white block max-w-sm mx-auto w-1/2 p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <div key={thread.id} className="bg-white block max-w-sm mx-auto w-1/2 p-6 border border-gray-200 rounded-lg shadow hover:bg-gray-100">
             <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{thread.title}</div>
             <div className="font-normal text-gray-700">{thread.id}</div>
         </div>
