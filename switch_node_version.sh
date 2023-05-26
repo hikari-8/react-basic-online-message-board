7e0340b35447b6ccbceebd2c91bce7e32919dc8e
hikari-8 <kanbarusuruga0127@gmail.com>
2023-05-26 19:15:48 +0900

    update/auto install nodev✨

--- switch_node_version.sh
+++ switch_node_version.sh
@@ -2,8 +2,8 @@
 set -e
 
 NODE_VERSION_FILE=".node-version"
-while ["$PWD"!="/"]; do
-  if[-f"$NODE_VERSION_FILE"]; then
+while [ "$PWD" != "/" ]; do
+  if [ -f "$NODE_VERSION_FILE" ]; then
     NODE_VERSION=$(cat "$NODE_VERSION_FILE")
     echo "Switching to NOde.js version $NODE_VERSION"
     nodebrew use $NODE_VERSION
