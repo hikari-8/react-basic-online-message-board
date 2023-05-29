86419c5b637e00e06c14e5836c073b9503b293e9
hikari-8 <kanbarusuruga0127@gmail.com>
2023-05-29 21:31:42 +0900

    update/ header with link to home✨

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { AllBoard } from './components/pages/allBoard';
 import { Routes, Route } from "react-router-dom";
 import { CreateThread } from './components/pages/createThread';
 import { ThreadDetails } from './components/pages/threadDetails';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header py-4">
+      <header className="py-4 bg-slate-50 shadow-sm text-lg font-bold ml-10">
+        <Link to="/">
         掲示板
+        </Link>
       </header>
       <Routes> {/*Routesで囲む*/}
         <Route path="/" element={ <AllBoard /> } />
